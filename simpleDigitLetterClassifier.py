06093930be3ddcbeb7d9be3ea5dcf3e34a96b0d8
duyboy135 <duyboy135@duyboy135.com>
2020-06-08 16:10:47 -0400

    fix code

--- simpleDigitLetterClassifier.py
+++ simpleDigitLetterClassifier.py
@@ -17,21 +17,25 @@ def featureExtraction(X, rate):
         X_data.append(features)
     return np.array(X_data)
 
-DIR = os.path.join("training_data", "output_securimage")
-LEFT = 2500
-RIGHT = 2500
-X,y,rate = collectTrainData(DIR, LEFT, RIGHT, False, 210)
-#X = featureExtraction(X, rate) #do featureExtraction using MFCC
-
+#collect data
+DIR_TRAIN = os.path.join("data", "securimage_digits_distorted", "train")
+DIR_TEST = os.path.join("data", "securimage_digits_distorted", "test")
+LEFT = 100
+RIGHT = 100
+print("Train_data location = " + DIR_TRAIN)
+print("Test_data location = " + DIR_TEST)
+X_train,y_train,rate = collectTrainData(DIR_TRAIN, LEFT, RIGHT, False)
+X_test, y_test, rate = collectTrainData(DIR_TEST, LEFT, RIGHT, False)
 #====================================================================================
 print("KNN with features = raw array of amplitudes:")
 # split the data into training and test sets\n",
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, stratify=y, random_state=0)
+#X_train, X_test, y_train, y_test = train_test_split(
+#    X, y, stratify=y, random_state=0)
 knn = KNeighborsClassifier(n_neighbors=1) #KNN algorithm
 knn.fit(X_train, y_train)
 print("Test set score of 1-nn: {:.4f}".format(knn.score(X_test, y_test)))
 
+"""
 #plot samples
 print("=> Plot samples array of amplitudes for each single digit......")
 nSample = 7
@@ -39,7 +43,7 @@ count = [0 for i in range(10)]
 image = [None for i in range(10*nSample)]
 plt.close()
 fig, axes = plt.subplots(10 , nSample , figsize =(7 , 7), subplot_kw={'xticks': (), 'yticks': ()})
-for signal,label in zip(X,y):
+for signal,label in zip(X_train,y_train):
     if count[label] < nSample:
         image[nSample*label + count[label]] = signal
         count[label] += 1
@@ -50,3 +54,4 @@ for img,ax in zip(image, axes.ravel()):
     ax.plot(times, img)
     ax.set_xlim(0, seconds)
 plt.show()
+"""
