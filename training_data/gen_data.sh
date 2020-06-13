f155e2756f27c653ad4ef397d38eaaf64fe49a8b
Sampriti Panda <sampritipanda@outlook.com>
2020-06-12 21:52:56 -0400

    Reduce data in repo and improve build script

--- training_data/gen_data.sh
+++ training_data/gen_data.sh
@@ -2,15 +2,19 @@
 
 mkdir -p ../data/securimage_digits/train
 mkdir -p ../data/securimage_digits/test
-php gen_data_securimage.php ../data/securimage_digits/train 4 1000
-php gen_data_securimage.php ../data/securimage_digits/test 4 500
+sed -i 's/0123456789abcdefghijklmnopqrstuvwxyz/0123456789/g' gen_data_securimage.php
+php gen_data_securimage.php ../data/securimage_digits/train 4 50
+php gen_data_securimage.php ../data/securimage_digits/test 4 10
 
 mkdir -p ../data/securimage_digits_distorted/train
 mkdir -p ../data/securimage_digits_distorted/test
-php gen_data_securimage.php ../data/securimage_digits_distorted/train 4 1000
-php gen_data_securimage.php ../data/securimage_digits_distorted/test 4 500
+sed -i 's/mt_rand(95/mt_rand(80/g' securimage/securimage.php
+php gen_data_securimage.php ../data/securimage_digits_distorted/train 4 50
+php gen_data_securimage.php ../data/securimage_digits_distorted/test 4 10
 
 mkdir -p ../data/securimage_all/train
 mkdir -p ../data/securimage_all/test
-php gen_data_securimage.php ../data/securimage_all/train 4 1000
-php gen_data_securimage.php ../data/securimage_all/test 4 500
+sed -i 's/mt_rand(80/mt_rand(95/g' securimage/securimage.php
+sed -i 's/0123456789/0123456789abcdefghijklmnopqrstuvwxyz/g' gen_data_securimage.php
+php gen_data_securimage.php ../data/securimage_all/train 4 50
+php gen_data_securimage.php ../data/securimage_all/test 4 10
